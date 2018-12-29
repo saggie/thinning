46a6182a53999ff46272ea9a896fe85341115558
saggie <16481948+saggie@users.noreply.github.com>
2018-12-29 15:38:23 +0900

    Update drag_and_drop.js
    Add "paste" support

--- javascripts/drag_and_drop.js
+++ javascripts/drag_and_drop.js
@@ -10,14 +10,26 @@ THIN.drag_and_drop = (function () {
     event.stopPropagation();
     event.preventDefault();
 
-    var files = event.dataTransfer.files;
+    let files = event.dataTransfer.files;
     THIN.sourceFile = files[0];
-
     THIN.main.execute();
   };
+
+  var onPaste = function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    let files = event.clipboardData.files;
+    if (files.length > 0) {
+      THIN.sourceFile = files[0];
+      THIN.main.execute();
+    }
+  }
 
   var dropZone = document.getElementById("drop_zone");
   dropZone.addEventListener("dragover", onDragOver, false);
   dropZone.addEventListener("drop", onDrop, false);
+
+  window.addEventListener("paste", onPaste, false);
 
 })();
